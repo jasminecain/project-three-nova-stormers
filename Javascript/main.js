6c367a265892d98562c3a19523e58a37e186517f
jordanberghofer <jordan.berghofer@gmail.com>
2017-07-31 18:34:04 -0500

    main.js updated added functionality

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,6 +1,10 @@
+//First half of the requirements for the assignment*********/
+
 let darkTheme = document.getElementById("checkbox1");
 let largeText = document.getElementById("checkbox2");
 let bodyBG = document.getElementById("bodyBG");
+let input = document.getElementById("inputField");
+
 
 darkTheme.addEventListener("change", function (){
     bodyBG.classList.toggle("darkT");
@@ -9,5 +13,52 @@ darkTheme.addEventListener("change", function (){
 largeText.addEventListener("change", function(){
     bodyBG.classList.toggle("largeT");
 });
+
+
+
+
+input.addEventListener("keyup", function(event){
+    if (event.keyCode === 13) {
+        document.getElementById("output").innerHTML = /*`${chatty}`*/  "test";
+    }
+});
+
+
+//**********************************************************/
+
+//                  Actual IIFE files
+
+//**********************************************************/
+
+
+
+        // Name all of the variables here
+{   
+    var chatty = {};
+    var output = document.getElementById("output");
+    var message = createElement("<p>");
+    message.innerHTML = "cats";
+    output.append(message);
+    output.remove(message);
+}
+
+
+
+
+// {
+//     let JSONfiles = [];
+
+//     jason.getJSONfiles = () => {
+//         return JSONfiles;
+//     }
+
+//     jason.addKind = (whatKind) => {
+//         JSONfiles.push(whatKind)
+//     }
+
+
+
+// }
+
 
 
