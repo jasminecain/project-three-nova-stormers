c20f119eea7094dadf4a488b683e7eb558a7ac52
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-01 15:37:14 -0500

    delete button for the JSON files complete

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,5 @@
+
+
 let dataRequest = new XMLHttpRequest();
 
 dataRequest.addEventListener('load', dataRequestComplete);
@@ -11,18 +13,32 @@ function dataRequestComplete(event) {
 }
 
 function showMsg(messages) {
-  let messageDiv = document.getElementById('output');
+  let output = document.getElementById('output');
   let messageData = '';
 
   for(item in messages) {
     let messageItem = messages[item];
     messageData += '<div id="card">';
     messageData +='<p>' + messageItem.msgID + '.' + messageItem.msgTxt + '</p>';
-    messageData +='<button>Delete</button>';
+    messageData +='<button value="delete">Delete</button>';
     messageData += '</div>';
   }
-  messageDiv.innerHTML += messageData;
+  output.innerHTML += messageData;
 }
+
+
+// Delete button functionality for JSON dlt Button
+let body = document.querySelector("body");
+
+body.addEventListener("click", function(event){
+    if (event.target.value === "delete") {
+            let card = event.target.parentNode;
+            console.log("event", card);
+            output.removeChild(card);
+    }
+});
+
+
 
 function dataRequestFailed(event) {
   console.log('dataFailed', event);
@@ -32,7 +48,7 @@ dataRequest.open('GET', 'messages.json');
 dataRequest.send();
 
 
-//First half of the requirements for the assignment*********/
+/*********Options for the Assignment*********/
 
 let darkTheme = document.getElementById("checkbox1");
 let largeText = document.getElementById("checkbox2");
@@ -79,48 +95,19 @@ input.addEventListener("keyup", function(event){
 
         //What the button will say and outputting that into the output
         msgDltBtn.innerHTML = "Delete";
-        msgDelete = msgDltBtn.name;
-
-            // this is framework variable for us to use "i" and the index/id for the entire kit and kaboodle
-        // msgDltBtn.id = i;
-        
+        msgDelete = msgDltBtn.name;        
 
         //"messageText" is the input that will be housed inside the "message" that will be input into the innerHTML 
         message.innerHTML = messageText;
-            //by creating `card ${i}`; we are creating a literal string of the id, cDiv. Because "i" is dynamic, in this case it is refering to line 83.
-        // cDiv.id = `card ${i}` ;
-        //     // this is incrimenting "i" by one after it has been "valued"
-        // i++;
 
         //Creating the eventListener that will delete the individual message on the message board
         msgDltBtn.addEventListener("click", function(event){
-            //this = song and dance
             
+            //this = song and dance
             let card = this.parentNode;
             output.removeChild(card);
-            
-
-            // //this is finding the card that has the same id as the msgDltBtn
-            // let card = document.getElementById(`card ${this.id}`);
-
-            // //to find the index of "card", we convert the nodeList to an array and call indexof"card" into that array.
-            // let nodeArray = Array.from(output.childNodes);
-            // let cardIndex = nodeArray.indexOf(card);
-
-            // console.log("cardIndex", cardIndex);
-            
-            // //we then use the index of the card to find that node in the "node list"
-            // let cardNode = output.childNodes.item(cardIndex);
-            
-            // //once we have a reference to that node, we can pass that to remove child within the parent element.
-            // output.removeChild(cardNode);
-
 
         });
-
-        /****** Creating the array in which all of the users inputs will be placed into ******/
-
-
 
         //this is running the function that is creating the message
         cDiv.append(message);
@@ -136,55 +123,13 @@ input.addEventListener("keyup", function(event){
 
 
 /******** This is the function to clear everything within the messageboard *********/
+
 let clearMessages = document.getElementById("clearMessages");
 
-clearMessages.addEventListener("click", function(event){
-        document.getElementById("output").innerHTML = "";
+    clearMessages.addEventListener("click", function(event){
+    document.getElementById("output").innerHTML = "";
 
 });
-
-
-
-/********* This is the function for the individual delete button *********/
-
-// let dltMsg = document.getElementById("dltBtn");
-
-// dltMsg.addEventListener("click", function(event){  
-//     document.getElementById("card").innerHTML = "";
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-// input.addEventListener("keyup", function(event){
-//     if (event.keyCode === 13) {
-//         var chatty = {};
-//         let messageText = input.value;
-//         console.log("message text", messageText);
-//         var output = document.getElementById("output");
-//         var message = document.createElement("p");
-//         var msgDltBtn = document.createElement("button");
-//         msgDltBtn.innerHTML = "Delete";
-//         msgDelete = msgDltBtn.name;
-//         var cDiv = document.createElement("div");
-//         message.innerHTML = messageText;
-//         cDiv.classList.add("card");
-//         cDiv.append(message);
-//         cDiv.append(msgDltBtn);
-//         output.append(cDiv);
-//     }
-// });
-
-
-
 
 
 //**********************************************************/
@@ -197,31 +142,7 @@ clearMessages.addEventListener("click", function(event){
 
         // Name all of the variables here
 
-// {   
+var chatty = {};
+var output = document.getElementById("output");
 
-    var chatty = {};
-    var output = document.getElementById("output");
-    var message = document.createElement("h2");
-
-
-    output.append(message);
-    // output.remove(message);
-// }
-
-
-
-
-// {
-//     let JSONfiles = [];
-
-//     jason.getJSONfiles = () => {
-//         return JSONfiles;
-//     }
-
-//     jason.addKind = (whatKind) => {
-//         JSONfiles.push(whatKind)
-//     }
-
-
-
-// }
+    // var message = document.createElement("h2");
